42f1ee26af90733b0a22ce1da7ed4e97254a522d
Juan Manuel Bouvier <juanmanuel.bouvier@adoos.com>
2016-10-30 20:14:08 -0300

    Created sign up screen without functionality for now.

--- app/src/main/java/com/fiuba/tallerii/lincedin/activities/SignUpActivity.java
+++ app/src/main/java/com/fiuba/tallerii/lincedin/activities/SignUpActivity.java
@@ -2,6 +2,9 @@ package com.fiuba.tallerii.lincedin.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.fiuba.tallerii.lincedin.R;
 
@@ -13,5 +16,30 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        setToolbar();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_empty, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_signup);
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null){
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
     }
 }
